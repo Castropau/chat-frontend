eced1d7f94ee524386482e5dca446265bb2ca079
Paulo Castro <castro.paulo5.00@gmail.com>
2025-11-28 15:10:11 +0800

    test241

--- src/utils/socket-server.ts
+++ src/utils/socket-server.ts
@@ -9,6 +9,7 @@ import { Server as IOServer, Socket } from "socket.io";
  const PORT = process.env.PORT || 4000;
 // const PORT = 5000;
 const app = express();
+
 app.use(express.json());
 
 const server = http.createServer(app);
