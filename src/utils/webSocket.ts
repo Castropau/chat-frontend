c24978810165e1505ac1791192bd6dde0cf71ef0
Paulo Castro <castro.paulo5.00@gmail.com>
2025-11-30 15:40:31 +0800

    test2

--- src/utils/webSocket.ts
+++ src/utils/webSocket.ts
@@ -81,12 +81,13 @@
 import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
-export function initSocket(url: string) {
+export function initSocket(url?: string) {
   if (!socket) {
-    socket = io(url, { transports: ["websocket"], reconnectionAttempts: 5 });
+    const SOCKET_URL = url || process.env.NEXT_PUBLIC_SOCKET_URL!;
+    socket = io(SOCKET_URL, { transports: ["websocket"], reconnectionAttempts: 5 });
   }
   return socket;
-} 
+}
 export function getSocket(): Socket {
   if (!socket) {
     socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
