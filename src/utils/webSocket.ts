a6943d911e6f1f15fb5b56e6aded741a5dfcfca5
Paulo Castro <castro.paulo5.00@gmail.com>
2025-11-30 15:59:35 +0800

    test

--- src/utils/webSocket.ts
+++ src/utils/webSocket.ts
@@ -1,86 +1,57 @@
-// // "use client";
-// // import { io, Socket } from "socket.io-client";
-
-// // let socket: Socket | null = null;
-
-// // export function initSocket(url: string) {
-// //   if (!socket) {
-// //     socket = io(url, {
-// //       transports: ["websocket"],
-// //       reconnectionAttempts: 5,
-// //     });
-// //   }
-// //   return socket;
-// // }
-
-// // export function getSocket() {
-// //   return socket;
-// // }
-
-// // export function closeSocket() {
-// //   if (socket) {
-// //     socket.disconnect();
-// //     socket = null;
-// //   }
-// // }
-// // "use client";
-// // import { io, Socket } from "socket.io-client";
-
-// // let socket: Socket | null = null;
-
-// // export const initSocket = (url: string) => {
-// //   if (!socket) {
-// //     socket = io(url, { transports: ["websocket"], reconnectionAttempts: 5 });
-// //   }
-// //   return socket;
-// // };
-
-// // export const getSocket = () => socket;
-// // export const closeSocket = () => {
-// //   socket?.disconnect();
-// //   socket = null;
-// // };
-
 // "use client";
 // import { io, Socket } from "socket.io-client";
 
 // let socket: Socket | null = null;
 
-// // export function initSocket(url: string) {
-// //   if (!socket) {
-// //     socket = io(url, { transports: ["websocket"], reconnectionAttempts: 5 });
-// //   }
-// //   return socket;
-// // }
-// export function initSocket(url?: string) {
+// export function initSocket(url: string) {
 //   if (!socket) {
-//     const SOCKET_URL = url || process.env.NEXT_PUBLIC_SOCKET_URL!;
-//     socket = io(SOCKET_URL, { transports: ["websocket"], reconnectionAttempts: 5 });
-//   }
-//   return socket;
-// }
-// // export function getSocket() {
-// //   return socket;
-// // }
-// export function getSocket(): Socket {
-//   if (!socket) {
-//     socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
-//       path: "/api/socket"
+//     socket = io(url, {
+//       transports: ["websocket"],
+//       reconnectionAttempts: 5,
 //     });
 //   }
 //   return socket;
 // }
+
+// export function getSocket() {
+//   return socket;
+// }
+
 // export function closeSocket() {
 //   if (socket) {
 //     socket.disconnect();
 //     socket = null;
 //   }
 // }
+// "use client";
+// import { io, Socket } from "socket.io-client";
+
+// let socket: Socket | null = null;
+
+// export const initSocket = (url: string) => {
+//   if (!socket) {
+//     socket = io(url, { transports: ["websocket"], reconnectionAttempts: 5 });
+//   }
+//   return socket;
+// };
+
+// export const getSocket = () => socket;
+// export const closeSocket = () => {
+//   socket?.disconnect();
+//   socket = null;
+// };
 
 "use client";
 import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
+
+// export function initSocket(url: string) {
+//   if (!socket) {
+//     socket = io(url, { transports: ["websocket"], reconnectionAttempts: 5 });
+//   }
+//   return socket;
+// }
 export function initSocket(url?: string) {
   if (!socket) {
     const SOCKET_URL = url || process.env.NEXT_PUBLIC_SOCKET_URL!;
@@ -88,14 +59,7 @@ export function initSocket(url?: string) {
   }
   return socket;
 }
-export function getSocket(): Socket {
-  if (!socket) {
-    socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
-      transports: ["websocket"],
-      reconnectionAttempts: 5,
-      withCredentials: true,
-    });
-  }
+export function getSocket() {
   return socket;
 }
 
@@ -105,3 +69,4 @@ export function closeSocket() {
     socket = null;
   }
 }
+
